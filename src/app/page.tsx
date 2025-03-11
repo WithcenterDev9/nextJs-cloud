f7547284a14b09e19c840bcee63ee68498b2de80
WithcenterDev6 <withcenterdev6@WithcenterDev6s-iMac.local>
2025-03-11 10:37:15 +0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="flex justify-center items-center h-screen text-white bg-green-500">
+      <h1 className="text-5xl ">This is testing</h1>
+    </section>
+  );
+}
